066b8390a94ba61e3887ec97f8d313223b9a35c6
Rob Vesse <rvesse@dotnetrdf.org>
2013-10-22 05:06:27 -0700

    Correct email address in Copyright headers

--- wiki-data-model/src/main/java/org/dotnetrdf/wiki/data/documents/formats/DocumentFormatRegistry.java
+++ wiki-data-model/src/main/java/org/dotnetrdf/wiki/data/documents/formats/DocumentFormatRegistry.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2013 dotNetRDF Project (dotnetrdf-developer@lists.sf.net)
+ * Copyright (c) 2013 dotNetRDF Project (dotnetrdf-develop@lists.sf.net)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
