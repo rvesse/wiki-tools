b3808e4519bb87aa07b2696e8e9d228b41964d7f
Rob Vesse <rvesse@dotnetrdf.org>
2013-10-22 05:02:05 -0700

    Fix license statements

---
+++ ensure-licenses.bat
@@ -0,0 +1,9 @@
+@echo off
+
+cd > temp.dir
+set /p pwd=<temp.dir
+rem temp.dir
+
+echo Directory to check is %pwd%
+
+LicenseChecker.exe -d "%pwd%" -exclude-exts "xml,class,properties,classpath,project,prefs,mf,jar,md,orig,txt,bat,dir" -exclude "\.hg|target" -license-search "dotNetRDF Project" -license-file "%pwd%\license-header.txt" -fix -overwrite
